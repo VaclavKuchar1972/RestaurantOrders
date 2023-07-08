ddb9b090db2e1e888225c6550074148389096454
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-07-08 15:31:20 +0200

    Po přidání třídy Waiter.

---
+++ src/com/certifikace/projekt1/Waiter.java
@@ -0,0 +1,31 @@
+package com.certifikace.projekt1;
+
+public class Waiter {
+
+    private String waiterFirstName;
+    private String waiterSecondName;
+    private String waiterIdentificationDocumentNumber;
+    private String waiterTypeOfEmploymentRelationship;
+
+    public Waiter(String waiterFirstName, String waiterSecondName, String waiterIdentificationDocumentNumber,
+                  String waiterTypeOfEmploymentRelationship) {
+        this.waiterFirstName = waiterFirstName;
+        this.waiterSecondName = waiterSecondName;
+        this.waiterIdentificationDocumentNumber = waiterIdentificationDocumentNumber;
+        this.waiterTypeOfEmploymentRelationship = waiterTypeOfEmploymentRelationship;
+    }
+
+    public String getWaiterFirstName() {return waiterFirstName;}
+    public void setWaiterFirstName(String waiterFirstName) {this.waiterFirstName = waiterFirstName;}
+    public String getWaiterSecondName() {return waiterSecondName;}
+    public void setWaiterSecondName(String waiterSecondName) {this.waiterSecondName = waiterSecondName;}
+    public String getWaiterIdentificationDocumentNumber() {return waiterIdentificationDocumentNumber;}
+    public void setWaiterIdentificationDocumentNumber(String waiterIdentificationDocumentNumber) {
+        this.waiterIdentificationDocumentNumber = waiterIdentificationDocumentNumber;
+    }
+    public String getWaiterTypeOfEmploymentRelationship() {return waiterTypeOfEmploymentRelationship;}
+    public void setWaiterTypeOfEmploymentRelationship(String waiterTypeOfEmploymentRelationship) {
+        this.waiterTypeOfEmploymentRelationship = waiterTypeOfEmploymentRelationship;
+    }
+
+}
