3f402577f1354f415f34ecdf69cd252d266238ea
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-07-08 11:42:19 +0200

    Po přidání Třídy RestaurantSettings.

---
+++ src/com/certifikace/projekt1/RestaurantSettings.java
@@ -0,0 +1,11 @@
+package com.certifikace.projekt1;
+
+public class RestaurantSettings {
+
+    public static String fileTables() {return "DB-Tables.txt";}
+    public static String fileTablesBackUp() {return "DB-TablesBackUp.txt";}
+
+    public static String delimiter() {return "; ";}
+
+
+}
