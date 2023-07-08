6a0d5e779169372fd1b65e38845f3ab66a1ad4af
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-07-08 11:29:40 +0200

    Po přidání Třídy Table.

--- src/com/certifikace/projekt1/Table.java
+++ src/com/certifikace/projekt1/Table.java
@@ -1,9 +1,34 @@
 package com.certifikace.projekt1;
 
+import java.time.format.DateTimeFormatter;
+
 public class Table {
 
+    private int tableNumber;
+    private String tableLocation;
+    private String tableSector;
+    private int tableCapacity;
 
+    public Table(int tableNumber, String tableLocation, String tableSector, int tableCapacity) {
+        this.tableNumber = tableNumber;
+        this.tableLocation = tableLocation;
+        this.tableSector = tableSector;
+        this.tableCapacity = tableCapacity;
+    }
 
+    public String getTableInfo () {
+        return "Číslo stolu: " + tableNumber + "   Umístění stolu: " + tableLocation
+                + "   Sektor umístění stolu pro grafický výstup na FrontEndu: " + tableSector +
+                "   Počet míst u stolu: " + tableCapacity;
+    }
 
+    public int getTableNumber() {return tableNumber;}
+    public void setTableNumber(int tableNumber) {this.tableNumber = tableNumber;}
+    public String getTableLocation() {return tableLocation;}
+    public void setTableLocation(String tableLocation) {this.tableLocation = tableLocation;}
+    public String getTableSector() {return tableSector;}
+    public void setTableSector(String tableSector) {this.tableSector = tableSector;}
+    public int getTableCapacity() {return tableCapacity;}
+    public void setTableCapacity(int tableCapacity) {this.tableCapacity = tableCapacity;}
 
 }
