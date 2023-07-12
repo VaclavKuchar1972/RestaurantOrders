588e5827fd0bed30348016e1acf958128cfb3ff3
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-07-12 19:39:11 +0200

    Po přidání možnosti třídění číšníků podle typu (kategorie) uzavřené pracovní smlouvy. Absolutně nedokončené!!!

---
+++ src/com/certifikace/projekt1/WaiterCategoryTypeOfEmploymentContract.java
@@ -0,0 +1,24 @@
+package com.certifikace.projekt1;
+
+public enum WaiterCategoryTypeOfEmploymentContract {
+    MER("hlavní"), SE("vedlejší"), BRIGADE("brigáda"), ATPW("dohoda o provedení práce"),
+    AOWA("dohoda o pracovní činnosti");
+
+//    private final String categoryName;
+
+//    WaiterCategory(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
+
+    WaiterCategoryTypeOfEmploymentContract(String s) {
+    }
+
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+
+
+
+
+
+}
