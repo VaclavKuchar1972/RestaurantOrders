4beacdcc98225e86fa677f2590425394d22c708b
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-07-22 20:44:21 +0200

    Po přidání základů třídy Dish a jejích  základních Getrů a Seterů a další doúpravě.

--- src/com/certifikace/projekt1/Dish.java
+++ src/com/certifikace/projekt1/Dish.java
@@ -1,5 +1,6 @@
 package com.certifikace.projekt1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.math.BigDecimal;
 
@@ -17,92 +18,43 @@ public class Dish {
     private int dishNumberOfNextPhotos;
     private List<String> dishNextPhoto;
 
+    public Dish() {this.dishNextCategory = new ArrayList<>(); this.dishNextPhoto = new ArrayList<>();}
 
-    public FoodCategory getDishRecomendedMainCategory() {
-        return dishRecomendedMainCategory;
-    }
-
+    public FoodCategory getDishRecomendedMainCategory() {return dishRecomendedMainCategory;}
     public void setDishRecomendedMainCategory(FoodCategory dishRecomendedMainCategory) {
         this.dishRecomendedMainCategory = dishRecomendedMainCategory;
     }
-
-    public int getDishNumberOfNextCategory() {
-        return dishNumberOfNextCategory;
-    }
-
+    public int getDishNumberOfNextCategory() {return dishNumberOfNextCategory;}
     public void setDishNumberOfNextCategory(int dishNumberOfNextCategory) {
         this.dishNumberOfNextCategory = dishNumberOfNextCategory;
     }
-
-    public List<FoodCategory> getDishNextCategory() {
-        return dishNextCategory;
-    }
-
-    public void setDishNextCategory(List<FoodCategory> dishNextCategory) {
-        this.dishNextCategory = dishNextCategory;
-    }
-
-    public String getDishTitle() {
-        return dishTitle;
-    }
-
-    public void setDishTitle(String dishTitle) {
-        this.dishTitle = dishTitle;
-    }
-
-    public int getDishRecommendedQuantity() {
-        return dishRecommendedQuantity;
-    }
-
+    public List<FoodCategory> getDishNextCategory() {return dishNextCategory;}
+    public void setDishNextCategory(List<FoodCategory> dishNextCategory) {this.dishNextCategory = dishNextCategory;}
+    public void addDishCategory(FoodCategory category) {dishNextCategory.add(category);}
+    public String getDishTitle() {return dishTitle;}
+    public void setDishTitle(String dishTitle) {this.dishTitle = dishTitle;}
+    public int getDishRecommendedQuantity() {return dishRecommendedQuantity;}
     public void setDishRecommendedQuantity(int dishRecommendedQuantity) {
         this.dishRecommendedQuantity = dishRecommendedQuantity;
     }
-
-    public String getDishRecommendedUnitOfQuantity() {
-        return dishRecommendedUnitOfQuantity;
-    }
-
+    public String getDishRecommendedUnitOfQuantity() {return dishRecommendedUnitOfQuantity;}
     public void setDishRecommendedUnitOfQuantity(String dishRecommendedUnitOfQuantity) {
         this.dishRecommendedUnitOfQuantity = dishRecommendedUnitOfQuantity;
     }
-
-    public BigDecimal getDishRecommendPrice() {
-        return dishRecommendPrice;
-    }
-
-    public void setDishRecommendPrice(BigDecimal dishRecommendPrice) {
-        this.dishRecommendPrice = dishRecommendPrice;
-    }
-
-    public int getDishEstimatedPreparationTime() {
-        return dishEstimatedPreparationTime;
-    }
-
+    public BigDecimal getDishRecommendPrice() {return dishRecommendPrice;}
+    public void setDishRecommendPrice(BigDecimal dishRecommendPrice) {this.dishRecommendPrice = dishRecommendPrice;}
+    public int getDishEstimatedPreparationTime() {return dishEstimatedPreparationTime;}
     public void setDishEstimatedPreparationTime(int dishEstimatedPreparationTime) {
         this.dishEstimatedPreparationTime = dishEstimatedPreparationTime;
     }
-
-    public String getDishMainPhoto() {
-        return dishMainPhoto;
-    }
-
-    public void setDishMainPhoto(String dishMainPhoto) {
-        this.dishMainPhoto = dishMainPhoto;
-    }
-
-    public int getDishNumberOfNextPhotos() {
-        return dishNumberOfNextPhotos;
-    }
-
+    public String getDishMainPhoto() {return dishMainPhoto;}
+    public void setDishMainPhoto(String dishMainPhoto) {this.dishMainPhoto = dishMainPhoto;}
+    public int getDishNumberOfNextPhotos() {return dishNumberOfNextPhotos;}
     public void setDishNumberOfNextPhotos(int dishNumberOfNextPhotos) {
         this.dishNumberOfNextPhotos = dishNumberOfNextPhotos;
     }
+    public List<String> getDishNextPhoto() {return dishNextPhoto;}
+    public void setDishNextPhoto(List<String> dishNextPhoto) {this.dishNextPhoto = dishNextPhoto;}
+    public void addDishNextPhoto(String photo) {dishNextPhoto.add(photo);}
 
-    public List<String> getDishNextPhoto() {
-        return dishNextPhoto;
-    }
-
-    public void setDishNextPhoto(List<String> dishNextPhoto) {
-        this.dishNextPhoto = dishNextPhoto;
-    }
 }
