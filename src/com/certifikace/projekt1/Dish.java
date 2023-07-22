6d80cf23594093fed57317ec9a02e8d777502498
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-07-22 17:15:25 +0200

    Po přidání základů třídy Dish.

--- src/com/certifikace/projekt1/Dish.java
+++ src/com/certifikace/projekt1/Dish.java
@@ -1,7 +1,20 @@
 package com.certifikace.projekt1;
 
+import java.math.BigDecimal;
+
 public class Dish {
 
+    private FoodCategory dishRecomendedMainCategory;
+    private int dishNumberOfNextCategory;
+    private FoodCategory dishNextCategory;
+    private String dishTitle;
+    private int dishRecommendedQuantity;
+    private String dishRecommendedUnitOfQuantity;
+    private BigDecimal dishRecommendPrice;
+    private int dishEstimatedPreparationTime;
+    private String dishMainPhoto;
+    private int dishNumberOfNextPhotos;
+    private String dishNextPhoto;
 
 
 
