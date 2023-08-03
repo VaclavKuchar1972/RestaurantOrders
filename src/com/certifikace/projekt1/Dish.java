304bed70ca0c73f00110ab3b862b10aaba975dfe
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-08-03 20:20:37 +0200

    Po kompletním ošetření chybovejch hlášek u všech situací, které by mohli nastat v Dish.

--- src/com/certifikace/projekt1/Dish.java
+++ src/com/certifikace/projekt1/Dish.java
@@ -3,8 +3,6 @@ package com.certifikace.projekt1;
 import java.util.List;
 import java.math.BigDecimal;
 
-import static com.certifikace.projekt1.RestaurantSettings.delimiter;
-
 public class Dish {
 
     private FoodCategory dishRecomendedMainCategory;
@@ -44,25 +42,45 @@ public class Dish {
     }
 
     public FoodCategory getDishRecomendedMainCategory() {return dishRecomendedMainCategory;}
-    public void setDishRecomendedMainCategory(FoodCategory dishRecomendedMainCategory) {
+    public void setDishRecomendedMainCategory(FoodCategory dishRecomendedMainCategory) throws RestaurantException {
+        // OŠETŘENÍ - kategorie může mít pouze velká písmena, číslice a znak "_" (vykřičník opět neguje podmínku),
+        // toto "^[A-Z0-9_]+$" mi poradil ChatGPT3.5, nevím jak bych se k tomu jinak rychle dopracoval, ale je jasné,
+        // že to bude dělat to co chci, takže je zbytečné to testovat
+        if (!dishRecomendedMainCategory.getName().matches("^[A-Z0-9_]+$")) {
+            throw new RestaurantException("Chyba: dishRecomendedMainCategory může obsahovat pouze velká písmena, "
+                    // takto do textového řetězce můžu vkládat uvozovky
+                    + "číslice a znak \"_\".");
+        }
         this.dishRecomendedMainCategory = dishRecomendedMainCategory;
     }
-    public int getDishNumberOfNextRecomendedCategory() {
-
-
-        return dishNumberOfNextRecomendedCategory;
-    }
-    public void setDishNumberOfNextRecomendedCategory(int dishNumberOfNextRecomendedCategory) {
+    public int getDishNumberOfNextRecomendedCategory() {return dishNumberOfNextRecomendedCategory;}
+    public void setDishNumberOfNextRecomendedCategory(int dishNumberOfNextRecomendedCategory)
+            throws RestaurantException {
+        if (dishNumberOfNextRecomendedCategory < 0) {
+            throw new RestaurantException("Chyba: dishNumberOfNextRecomendedCategory nesmí být menší než nula.");
+        }
         this.dishNumberOfNextRecomendedCategory = dishNumberOfNextRecomendedCategory;
     }
     public List<FoodCategory> getDishNextRecomendedCategory() {return dishNextRecomendedCategory;}
-    public void setDishNextRecomendedCategory(List<FoodCategory> dishNextRecomendedCategory) {
+    public void setDishNextRecomendedCategory(List<FoodCategory> dishNextRecomendedCategory)
+            throws RestaurantException {
+        // OŠETŘENÍ - stejné jako pro dishRecomendedMainCategory
+        for (FoodCategory category : dishNextRecomendedCategory) {
+            if (!category.getName().matches("^[A-Z0-9_]+$")) {
+                throw new RestaurantException("Chyba: dishRecomendedMainCategory může obsahovat pouze velká písmena, "
+                        + "číslice a znak \"_\".");
+            }
+        }
         this.dishNextRecomendedCategory = dishNextRecomendedCategory;
     }
     public String getDishTitle() {return dishTitle;}
     public void setDishTitle(String dishTitle) {this.dishTitle = dishTitle;}
     public int getDishRecommendedQuantity() {return dishRecommendedQuantity;}
-    public void setDishRecommendedQuantity(int dishRecommendedQuantity) {
+    public void setDishRecommendedQuantity(int dishRecommendedQuantity) throws RestaurantException {
+        // OŠETŘENÍ - Restaurace nemůže za peníze prodávat nulové množství čehokoli
+        if (dishRecommendedQuantity < 1) {
+            throw new RestaurantException("Chyba: dishRecommendedQuantity je menší než 1.");
+        }
         this.dishRecommendedQuantity = dishRecommendedQuantity;
     }
     public String getDishRecommendedUnitOfQuantity() {return dishRecommendedUnitOfQuantity;}
@@ -70,15 +88,32 @@ public class Dish {
         this.dishRecommendedUnitOfQuantity = dishRecommendedUnitOfQuantity;
     }
     public BigDecimal getDishRecommendPrice() {return dishRecommendPrice;}
-    public void setDishRecommendPrice(BigDecimal dishRecommendPrice) {this.dishRecommendPrice = dishRecommendPrice;}
-    public int getDishEstimatedPreparationTime() {return dishEstimatedPreparationTime;}
-    public void setDishEstimatedPreparationTime(int dishEstimatedPreparationTime) {
+    public void setDishRecommendPrice(BigDecimal dishRecommendPrice) throws RestaurantException {
+        // OŠETŘENÍ - Restaurace nemůže vracet peníze za objednávky klientů, může jim to ale dát zadarmo, např., když
+        // na takovou položku udělá restaurace akci nebo si prostě majitel usmyslí, že chleby k polévce budou zadarmo
+        // - a hotovo (rozhodnutí managementu)
+        if (dishRecommendPrice.compareTo(BigDecimal.ZERO) < 0) {
+            throw new RestaurantException("Chyba: dishRecommendPrice nesmí mít zápornou hodnotu.");
+        }
+        this.dishRecommendPrice = dishRecommendPrice;}
+    public int getDishEstimatedPreparationTime() throws RestaurantException {return dishEstimatedPreparationTime;}
+    public void setDishEstimatedPreparationTime(int dishEstimatedPreparationTime) throws RestaurantException {
+        // OŠETŘENÍ - opravdu nejde nic udělat za nulový čas, tedy lze - další nic :-)
+        if (dishEstimatedPreparationTime < 1 ) {
+            throw new RestaurantException("Chyba: dishEstimatedPreparationTime nesmí být menší než 1.");
+        }
         this.dishEstimatedPreparationTime = dishEstimatedPreparationTime;
     }
     public String getDishMainPhoto() {return dishMainPhoto;}
-    public void setDishMainPhoto(String dishMainPhoto) {this.dishMainPhoto = dishMainPhoto;}
+    public void setDishMainPhoto(String dishMainPhoto) {
+        if (dishMainPhoto == null || dishMainPhoto.isEmpty()) {this.dishMainPhoto = "blank";}
+        else {this.dishMainPhoto = dishMainPhoto;}
+    }
     public int getDishNumberOfNextPhotos() {return dishNumberOfNextPhotos;}
-    public void setDishNumberOfNextPhotos(int dishNumberOfNextPhotos) {
+    public void setDishNumberOfNextPhotos(int dishNumberOfNextPhotos) throws RestaurantException {
+        if (dishNumberOfNextPhotos < 0) {
+            throw new RestaurantException("Chyba: dishNumberOfNextPhotos nesmí být menší než 0.");
+        }
         this.dishNumberOfNextPhotos = dishNumberOfNextPhotos;
     }
     public List<String> getDishNextPhoto() {return dishNextPhoto;}
