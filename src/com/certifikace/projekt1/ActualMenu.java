8302bc03f249c3700a5b00b29888b139a1fa8d3a
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-08-26 18:14:04 +0200

    Po přidání základů třídy ActualMenu.

---
+++ src/com/certifikace/projekt1/ActualMenu.java
@@ -0,0 +1,25 @@
+package com.certifikace.projekt1;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ActualMenu {
+
+
+    private FoodCategory amMainCategory;
+    private int amNumberOfNextCategory;
+    private List<FoodCategory> amNextRecomendedCategory;
+    private String amTitle;
+    private int amQuantity;
+    private String amUnitOfQuantity;
+    private BigDecimal amPrice;
+    private int amPreparationTime;
+    private String amMainPhoto;
+    private int amNumberOfNextPhotos;
+    private List<String> amNextPhoto;
+
+
+
+
+
+}
